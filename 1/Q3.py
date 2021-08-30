1eb78f3a779ddc214efe7fec27fdcfe6e2b896cb
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-08-30 15:52:55 +0530

    bilinear interpolation done (not for float values)

--- 1/Q3.py
+++ 1/Q3.py
@@ -8,37 +8,42 @@ import numpy as np
 # img = Image.open('x5.bmp')
 # matrix = np.asarray(img)
 matrix = np.array([[5,10],[10,20]])
+# matrix = np.array([[1,4,7],[10,13,16],[19,22,25]])
 
 #define the interpolation factor
-interpolationFactor = 2
+c = 2
 
 M1 = len(matrix)
 N1 = len(matrix[0])
 
-print(matrix)
+padded_matrix = np.ones((M1+1,N1+1))*0
 
-M2 = int(interpolationFactor*M1)
-N2 = int(interpolationFactor*N1)
+for i in range(M1):
+	for j in range(N1):
+		padded_matrix[i][j] = matrix[i][j]
+
+# print(matrix)
+# print()
+# print(padded_matrix)
+# print()
+
+M2 = c*(M1)
+N2 = c*(N1)
 
 new_matrix = np.ones((M2, N2))*-1
 
 #Create Output interpolated matrix structure
 for i in range(M1):
 	for j in range(N1):
-		new_matrix[int(i*interpolationFactor)][int(j*interpolationFactor)] = matrix[i][j]
+		new_matrix[i*c][j*c] = matrix[i][j]
 
 
-M_X = int(i*interpolationFactor)
-M_Y = int(j*interpolationFactor)
-
-print()
-
-for i in range(M_X+1):
-	for j in range(M_Y+1):
+#filling in the output matirx
+for i in range(M2):
+	for j in range(N2):
 		if new_matrix[i][j]==-1:
-
-			x = i/interpolationFactor
-			y = j/interpolationFactor
+			x = i/c
+			y = j/c
 
 			if ceil(x)!=x:
 				x1 = floor(x)
@@ -72,28 +77,23 @@ for i in range(M_X+1):
 				]
 
 			Y = [
-					[matrix[x1][y1]],
-					[matrix[x1][y2]],
-					[matrix[x2][y2]],
-					[matrix[x2][y1]],
+					[padded_matrix[x1][y1]],
+					[padded_matrix[x1][y2]],
+					[padded_matrix[x2][y2]],
+					[padded_matrix[x2][y1]],
 				]
 
 			A = np.dot(np.linalg.inv(X), Y)
 
 			new_matrix[i][j] = np.dot(np.array([x,y,x*y,1]),A)
 
-for i in range(M_X+1):
-	for j in range(M_Y+1, len(new_matrix[0])):
-		new_matrix[i][j] = new_matrix[i][j-1]
-
-for j in range(len(new_matrix[0])):
-	for i in range(M_X+1, len(new_matrix)):
-		new_matrix[i][j] = new_matrix[i-1][j]
 
 # img = Image.fromarray(new_matrix)
-# # img.save('test.png')
+# img.save('test.png')
 # img.show()
-# print("process ended")
-print(new_matrix)
+# print("Output")
+# print(new_matrix)
 
+print("process ended")
+print(new_matrix)
 
