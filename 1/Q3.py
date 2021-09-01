b23a64f98fdf7af530df2d9ab6fd5ee68931ddd9
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-09-01 15:41:17 +0530

    Update Q3.py
    Input statements added

--- 1/Q3.py
+++ 1/Q3.py
@@ -1,43 +1,52 @@
+"""
+Name: Kesar Shrivastava
+Roll number: 2019051
+"""
+
 # Import the necessary libraries
 from math import *
 from PIL import Image
 import numpy as np
 
+# input the path of the image
+path = input("Enter the path of the image: ")
+
 # load the image and convert into
 # numpy array
-img = Image.open('assign1.jpg')
+img = Image.open(path)
 matrix = np.asarray(img)
+
 # matrix = np.array([[5,10],[10,20]])
 # matrix = np.array([[1,4,7],[10,13,16],[19,22,25]])
 
-#define the interpolation factor
-c = 2
+# define the interpolation factor
+c = input("Enter the interpolation factor: ")
+c = int(c)
+# c = 2
 
+# Show the input image
+img.show()
+
+# Print the entered interpolation factor
+print("The interpolation factor is: ", end = "")
+print(c)
+
+# Define the number of rows and columns
 M1 = len(matrix)
 N1 = len(matrix[0])
 
 padded_matrix = np.ones((M1+1,N1+1))*0
 
+# pad with 0s for the last row and the last column 
 for i in range(M1):
 	for j in range(N1):
 		padded_matrix[i][j] = matrix[i][j]
 
-# print(matrix)
-# print()
-# print(padded_matrix)
-# print()
-
+# Create the output matrix
 M2 = round(c*(M1))
 N2 = round(c*(N1))
 
 new_matrix = np.ones((M2, N2))*-1
-
-#Create Output interpolated matrix structure
-
-# for i in range(M1):
-# 	for j in range(N1):
-# 		new_matrix[i*c][j*c] = matrix[i][j]
-
 
 #filling in the output matirx
 for i in range(M2):
@@ -88,13 +97,9 @@ for i in range(M2):
 
 			new_matrix[i][j] = np.dot(np.array([x,y,x*y,1]),A)
 
-# img.save('test.png')
+# Form the image and show
+img_new = Image.fromarray(new_matrix)
+img_new.show()
 
-img = Image.fromarray(new_matrix)
-img.show()
-# print("Output")
-# print(new_matrix)
-
-# print("process ended")
-# print(new_matrix)
+print("Process completed")
 
