34445c095cd3c24e4624520bd96cd49268ae7153
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-10-12 09:39:18 +0530

    Q3 done

--- 3.py
+++ 3.py
@@ -17,9 +17,8 @@ matrix = np.asarray(img)
 M = len(matrix)
 N = len(matrix[0])
 
+# the box filter
 convolution_matrix = np.ones((9,9))*(1/81)
-
-new_matrix = np.ones((M+9-1,N+9-1))*-1
 
 # padded image matrix
 padded_matrix = np.ones((M+9-1,N+9-1))*0
@@ -41,45 +40,29 @@ for i in range(9):
 dft_padded_matrix = np.fft.fft2(padded_matrix)
 dft_convolution_matrix = np.fft.fft2(convolution_padded_matrix)
 
-print(dft_padded_matrix)
-print(dft_convolution_matrix)
-
+# the elementwise multiplication
 elementwise_multiplied = np.ones((M+9-1,N+9-1))*-1
-
-# multiplying elementwise
-# for i in range(M+9-1):
-#     for j in range(N+9-1):
-#         elementwise_multiplied[i][j] = dft_convolution_matrix[i][j]*dft_padded_matrix[i][j]
-
 elementwise_multiplied = np.multiply(dft_convolution_matrix,dft_padded_matrix)
-
-print(elementwise_multiplied)
 
 # computing the inverse dft
 idft = np.fft.ifftn(elementwise_multiplied)
 
-print("element", elementwise_multiplied)
-
 # taking the real part of the matrix
 real_idft = idft.real
 
+# the output matrix
 new_matrix = np.ones((M,N))*-1
 
+# Transferring the value to the new matrix
 for i in range(M):
     for j in range(N):
         new_matrix[i][j] = round(real_idft[i][j])
 
-# output_matrix = np.ones((M,N))*-1
-# for i in range(M):
-#     for j in range(N):
-#         output_matrix[i][j] = new_matrix[i][j]
-
+# the output image
 img_new = Image.fromarray(new_matrix)
-
 img_new.show()
 
+# covolution done using in-built function and output shown
 output_matrix = signal.convolve2d(matrix,convolution_matrix)
-
 img_new_ = Image.fromarray(output_matrix)
-
 img_new_.show()
