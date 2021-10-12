b39f6b6669939388e44c824d72b88a4c13f6776e
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-10-12 12:29:54 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,15 @@
+# Import the necessary libraries
+from math import *
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import signal
+
+# input the path of the image
+path = 'camera.jpg'
+
+# load the image and convert into
+# numpy array
+img = Image.open(path)
+img.show()
+matrix = np.asarray(img)
