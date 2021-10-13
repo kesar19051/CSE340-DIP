a0f5c96283a349890b0f8910f3e9b111e5bacf11
Kesar Shrivastava <kesar19051@iiitd.ac.in>
2021-10-13 17:36:08 +0530

    Q4 attempted but wrong

---
+++ 4.py
@@ -0,0 +1,163 @@
+# Import the necessary libraries
+from math import *
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import signal
+
+# input the path of the image
+path = 'hello.jpg'
+
+# load the image and convert into
+# numpy array
+img = Image.open(path)
+img.show()
+matrix = np.asarray(img)
+
+# M = number of rows; N = number of columns
+M = len(matrix)
+N = len(matrix[0])
+
+# the padded matrix
+padded_matrix = np.ones((2*M,2*N))*0
+
+# padding the image to 2M X 2N
+for i in range(M):
+    for j in range(N):
+        padded_matrix[i][j] = matrix[i][j]
+
+# display padded image
+img = Image.fromarray(padded_matrix)
+img.show()
+
+# the centered 2d dft of image
+centered_2d_dft = np.ones((2*M,2*N))*0
+
+# computing the centered dft by multiplying with (-1)^(n+m)
+for i in range(2*M):
+    for j in range(2*N):
+        centered_2d_dft[i][j] = padded_matrix[i][j]*pow(-1,i+j)
+
+img = Image.fromarray(centered_2d_dft)
+img.show()
+
+# finding the dft
+dft_image_matrix = np.fft.fft2(centered_2d_dft)
+
+# for plotting the magnitude spectrum
+magnitude_spectrum = np.ones((2*M,2*N))*0
+
+# filling the values of magnitude spectrum
+for i in range(2*M):
+    for j in range(2*N):
+        magnitude_spectrum[i][j] = int(round(abs(dft_image_matrix[i][j])))
+
+# finding the min and max value in the matrix for scaling
+max = np.amax(magnitude_spectrum)
+min = np.amin(magnitude_spectrum)
+
+# scaling the pixel values
+for i in range(2*M):
+    for j in range(2*N):
+        magnitude_spectrum[i][j] = ((magnitude_spectrum[i][j]-min)/(max-min))*255
+
+# display the magnitude spectrum
+img = Image.fromarray(magnitude_spectrum)
+img.show()
+
+# defining the filter
+filter1 = np.ones((2*M,2*N))*0
+filter2 = np.ones((2*M,2*N))*0
+
+# defining D(u,v)
+def Duv(i,j):
+    i = pow(i-256,2)
+    j = pow(j-256,2)
+    return pow(i+j,0.5)
+
+# creating the filter
+for i in range(2*M):
+    for j in range(2*N):
+        duv = Duv(i,j)
+        if duv<=95:
+            filter1[i][j] = 1
+        else:
+            filter1[i][j] = 0
+
+# creating another filter
+for i in range(2*M):
+    for j in range(2*N):
+        duv = Duv(i,j)
+        if duv<=85:
+            filter2[i][j] = 1
+        else:
+            filter2[i][j] = 0
+
+filter = filter1-filter2
+
+filter_to_show = np.ones((2*M,2*N))*0
+
+max = np.amax(filter)
+min = np.amin(filter)
+
+for i in range(2*M):
+    for j in range(2*N):
+        filter_to_show[i][j] = ((filter[i][j]-min)/(max-min))*255
+
+img = Image.fromarray(filter_to_show)
+img.show()
+
+one = np.ones((2*M,2*N))*1
+
+filter = one-filter
+
+filter_to_show = np.ones((2*M,2*N))*0
+
+max = np.amax(filter)
+min = np.amin(filter)
+
+for i in range(2*M):
+    for j in range(2*N):
+        filter_to_show[i][j] = ((filter[i][j]-min)/(max-min))*255
+
+img = Image.fromarray(filter_to_show)
+img.show()
+
+# taking elementwise multiplication
+elementwise_multiplied = np.multiply(dft_image_matrix,filter)
+
+max = np.amax(elementwise_multiplied)
+min = np.amin(elementwise_multiplied)
+
+elementwise_multiplied_show = np.ones((2*M,2*N))*0
+
+for i in range(2*M):
+    for j in range(2*N):
+        elementwise_multiplied_show[i][j] = ((abs(elementwise_multiplied[i][j])-min)/(max-min))*255
+
+img = Image.fromarray(elementwise_multiplied_show)
+img.show()
+
+# computing the inverse dft
+idft = np.fft.ifftn(elementwise_multiplied)
+
+# taking the real part of the matrix
+real_idft = idft.real
+
+# centering it
+for i in range(2*M):
+    for j in range(2*N):
+        real_idft[i][j] = real_idft[i][j]*pow(-1,i+j)
+
+img = Image.fromarray(real_idft)
+img.show()
+
+# cropping the final image
+output_image = np.ones((M,N))*0
+
+for i in range(M):
+    for j in range(N):
+        output_image[i][j] = real_idft[i][j]
+
+img = Image.fromarray(output_image)
+img.show()
